f9ef9867004e5bcb285d0dadbe417672b5f17cd5
ABC-User <julianhsc@outlook.com>
2020-09-14 18:13:43 +0800

    Tutorial 162 completed.

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level timer: seconds")]
+    [SerializeField] float levelTime = 10;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+
+        if(timerFinished)
+        {
+            Debug.Log("level timer expired");
+
+
+        }
+
+    }
+}
