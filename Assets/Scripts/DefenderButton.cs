86174e097703c80e51ccd5069860563c2822f844
ABC-User <julianhsc@outlook.com>
2020-09-18 17:35:25 +0800

    Tutorial 174 completed. Quad removed. Attacker, defender objects brightened.

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,10 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] Defender defenderPrefab;
+
+    private void Start()
+    {
+        LabelButtonWithCost();
+
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if(!costText)
+        {
+            Debug.LogError(name + " has no cost text, add some!");
+
+        }
+
+        else
+
+        {
+            costText.text = defenderPrefab.GetStarCost().ToString();
+
+
+        }
+
+
+    }
 
     private void OnMouseDown()
     {
@@ -20,12 +48,7 @@ public class DefenderButton : MonoBehaviour
     }
 
 
-    /*
-    void Start()
-    {
-        
-    }
-
+    /*    
     // Update is called once per frame
     void Update()
     {
